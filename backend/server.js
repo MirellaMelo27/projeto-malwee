76068c443813a4928ca29f945066178b94b6ae42
mihh_ml <mirellamelo100@gmail.com>
2025-09-26 15:57:58 -0300

    up

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 const mysql = require('mysql2');
-
 const express = require('express');
 const cors =  require('cors');
 
 const app = express();
-
 const porta = 3000;
+
+app.use(cors());
+
+const connection = mysql.createConnection({
+  host: 'localhost',       // Servidor do MySQL
+  user: 'root',     // Usuário do MySQL
+  password: '',   // Senha do MySQL
+  database: 'planilha' // AQUI você coloca o nome do banco
+});
 
 connection.connect((erro) => {
     if (erro) {
@@ -15,23 +22,15 @@ connection.connect((erro) => {
     console.log('Conectado ao MySQL!');
   });
 
-app.get('/tabela', (req, res) => {
-    connection.query('select * from data', (erro, resultado) => {
-        if (erro) {
-            console.log("Erro ", erro )
-            return
-        } 
-        res.send(resultado)
-    })
-})
-
-
-const connection = mysql.createConnection({
-  host: 'localhost',       // Servidor do MySQL
-  user: 'root',     // Usuário do MySQL
-  password: '',   // Senha do MySQL
-  database: 'planilha' // AQUI você coloca o nome do banco
-});
+  app.get('/tabela', (req, res) => {
+    connection.query('SELECT * FROM data', (erro, resultado) => {
+      if (erro) {
+        console.error("Erro no MySQL:", erro);
+        return res.status(500).json({erro: 'Erro ao consultar banco'});
+      }
+      res.json(resultado);
+    });
+  });
 
 app.listen(porta, ()  => {
     console.log("http://localhost:3000")
