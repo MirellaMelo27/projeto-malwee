b3380a07341cefa4e72d0da06e34e9a0302c6ab5
ant <90523840+ant762@users.noreply.github.com>
2025-09-29 15:49:26 -0300

    integração js 2
    feito

--- frontend/script.js
+++ frontend/script.js
@@ -1,31 +1,10 @@
-fetch('http://localhost:3000/tabela')
+async function loadView(view) {
+  const res = await fetch(`views/${view}.html`);
+  const html = await res.text();
+  document.getElementById("content").innerHTML = html;
+}
 
-.then(response => {
-    if(!response.ok){
-        throw new Error('Erro ao buscar os produtos')
-    }
-    return response.json()
-})
+document.getElementById("btn-dashboard").addEventListener("click", () => loadView("dashboard"));
+document.getElementById("btn-infos").addEventListener("click", () => loadView("informacoes"));
 
-.then(tabelas => {
-
-    console.log(tabelas)
-    const info = document.getElementById('infos');
-
-    tabelas.slice(0,500).forEach(item => {
-      info.innerHTML += `
-        <tr>
-          <td>${item["Data (AAAA-MM-DD HH:MM:SS)"]}</td>
-          <td>${item["Tipo Tecido"]}</td>
-          <td>${item["Tipo de Saida"]}</td>
-          <td>${item["Numero da tarefa"]}</td>
-          <td>${item["Quantidade de Tiras"]}</td>
-          <td>${item["Metros Produzidos"]}</td>
-          <td>${item["Tarefa completa?"]}</td>
-        </tr>
-      `;
-
-      console.log
-    });
-  })
-  .catch(err => console.error(err));
+loadView("dashboard");
