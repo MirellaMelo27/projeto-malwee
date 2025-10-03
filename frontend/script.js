625f740166e3756397f69395d9644708b3f32536
PedroGoulartbranco <pgoulartbranco@gmail.com>
2025-10-03 13:11:44 -0300

    a

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,25 @@
+
+
+
 async function loadView(view) {
   const res = await fetch(`views/${view}.html`);
   const html = await res.text();
   document.getElementById("content").innerHTML = html;
+
+  fetch('http://localhost:3000/tabela')
+  .then(response => { 
+    if (!response.ok) {
+      throw new Error('Erro ao buscar os produtos')
+    } 
+    return response.json();
+  })
+  .then(tabelas => { 
+    console.log(tabelas);
+
+    tabelas.slice(0, 50).forEach(item => {
+    });
+  })
+  .catch(err => console.error(err));
 }
 
 document.getElementById("btn-dashboard").addEventListener("click", () => loadView("dashboard"));
